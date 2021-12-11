5c89026c67f70fc6236f71b9af40e3e0a1a807f0
Ian Wakeling <ian@lihb.me.uk>
2021-12-11 10:45:31 +0000

    Make fonts cachable and improve font loader interface

--- ttf.h
+++ ttf.h
@@ -49,12 +49,48 @@ namespace sdl
       return std::make_unique<lib>();
     }
 
-    /** std::unique_ptr wrapper for TTF_Font */
-    using font = std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)>;
+    /** std::shared_ptr wrapper for TTF_Font */
+    using font = std::shared_ptr<TTF_Font>;
 
-    /** Creates a TTF font object owned by the returned unique_ptr */
+    /** std::weak_ptr for TTF_Font. This can be used to implement a simple font
+        cache
+    */
+    using font_cache = std::weak_ptr<TTF_Font>;
+
+    /** Creates a TTF font object from a font name
+
+        The font is owned by the returned unique_ptr
+    */
     template<class ...Ts>
-    font open_font(Ts... args)
+    font open_font(std::string const& font_name, Ts... args)
+    {
+      auto config = FcInitLoadConfigAndFonts();
+      auto pat = FcNameParse(reinterpret_cast<FcChar8 const*>(font_name.c_str()));
+      FcConfigSubstitute(config, pat, FcMatchPattern);
+      FcDefaultSubstitute(pat);
+
+      auto result = FcResultNoMatch;
+      auto match = FcFontMatch(config, pat, &result);
+      std::string font_file;
+      if( result == FcResultMatch && match != nullptr )
+      {
+        FcChar8* file_name = NULL;
+        if( FcPatternGetString(match, FC_FILE, 0, &file_name) == FcResultMatch )
+        {
+          font_file = reinterpret_cast<char const*>(file_name);
+        }
+        FcPatternDestroy(match);
+      }
+      FcPatternDestroy(pat);
+      return font(TTF_OpenFont(font_file.c_str(), args...), TTF_CloseFont);
+    }
+
+    /** Creates a TTF font object from a font file name
+
+        The font is owned by the returned unique_ptr
+    */
+    template<class ...Ts>
+    font open_font_file(Ts... args)
     {
       return font(TTF_OpenFont(args...), TTF_CloseFont);
     }
@@ -77,29 +113,6 @@ namespace sdl
     {
       return surface(TTF_RenderUTF8_Blended(f.get(), text.c_str(), args...),
                      SDL_FreeSurface);
-    }
-
-    std::string GetFontFile(std::string const& fontName)
-    {
-      std::string fontFile;
-      auto config = FcInitLoadConfigAndFonts();
-      auto pat = FcNameParse(reinterpret_cast<FcChar8 const*>(fontName.c_str()));
-      FcConfigSubstitute(config, pat, FcMatchPattern);
-      FcDefaultSubstitute(pat);
-
-      auto result = FcResultNoMatch;
-      auto font = FcFontMatch(config, pat, &result);
-      if( result == FcResultMatch && font != nullptr )
-      {
-        FcChar8* fileName = NULL;
-        if( FcPatternGetString(font, FC_FILE, 0, &fileName) == FcResultMatch )
-        {
-          fontFile = reinterpret_cast<char const*>(fileName);
-        }
-        FcPatternDestroy(font);
-      }
-      FcPatternDestroy(pat);
-      return fontFile;
     }
   }
 }
